aa3d4485f9be9accc0620c4fc1723086439ea017
Aleks B <aleksandir@me.com>
2023-11-13 21:09:32 +1030

    Add calculator operations to app.py
    commented out plan

---
+++ app.py
@@ -0,0 +1,9 @@
+# calculator ill do the following operations:
+# 1. Addition
+# 2. Subtraction
+# 3. Multiplication
+# 4. Division
+# 5. Square (2) this will square the number already on the screen
+# 6. percentage (this will divide the number on the screen by 100)
+# 7. square root (this will find the square root of the number on the screen)
+# 8. clear (this will clear the screen)
